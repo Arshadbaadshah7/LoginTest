0777c4baa513958d6eac5465bc2de1b19f0953f0
Arshadbaadshah <arshad.pkl@terralogic.com>
2025-12-11 20:02:39 +0530

    Add Loginunstoppable as a part of main repo

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+}
